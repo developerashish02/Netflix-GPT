837db5b08a3f7a4fb1d804cf4862df41f4714baf
Ashish Gaikwad <developerashish02@gmail.com>
2024-04-20 00:48:15 +0530

    fix minor issues

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,16 +1,57 @@
 import { Formik } from "formik";
 import { useState } from "react";
 import { validateValues } from "../utils/validation";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase";
 
 const LogIn = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [isValidUser, setIsValidUser] = useState(null);
   const values = {
     email: "",
     password: "",
     firstName: "",
   };
 
-  const handleSubmit = (values, { setSubmitting }) => {};
+  const handleToggle = (resetForm) => {
+    setIsSignIn(!isSignIn);
+    resetForm();
+    setIsValidUser(null);
+  };
+
+  const handleSubmitForm = (values, { setSubmitting, resetForm }) => {
+    const { email, password } = values;
+    if (!isSignIn) {
+      // sign in form
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          setIsValidUser(null);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setIsValidUser(errorMessage);
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setIsValidUser(errorMessage);
+        });
+    }
+    setSubmitting(true);
+    resetForm();
+  };
 
   return (
     <div className="bg-black bg-opacity-85 w-full md:w-[80%] lg:w-[30%]  text-white px-4 sm:px-8 md:px-12 py-6 md:py-8 mx-auto rounded-md">
@@ -18,7 +59,7 @@ const LogIn = () => {
       <Formik
         initialValues={values}
         validate={validateValues}
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmitForm}
       >
         {({
           handleChange,
@@ -28,6 +69,7 @@ const LogIn = () => {
           handleSubmit,
           errors,
           touched,
+          resetForm,
         }) => (
           <div className="container">
             <form className="py-6 grid-cols-1 gap-4 " onSubmit={handleSubmit}>
@@ -68,7 +110,10 @@ const LogIn = () => {
                 {errors.password && touched.password && errors.password}
               </p>
 
+              <p className="text-red-500 my-4 ">{isValidUser} </p>
+
               <button
+                type="submit"
                 disabled={isSubmitting}
                 className="py-2 px-4 bg-[#E50914] hover:bg-red-500 w-full mt-6 rounded-md font-semibold"
               >
@@ -79,7 +124,7 @@ const LogIn = () => {
                 {isSignIn ? "New to Netflix?" : "Get Started!"}
                 <span
                   className="cursor-pointer font-bold hover:underline ml-1"
-                  onClick={() => setIsSignIn(!isSignIn)}
+                  onClick={() => handleToggle(resetForm)}
                 >
                   {isSignIn ? "Sign up now." : "Sign In now"}
                 </span>
