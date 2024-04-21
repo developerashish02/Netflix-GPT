734c6772224ac0bf6f3c66d7b7f815f4ab542807
Ashish Gaikwad <developerashish02@gmail.com>
2024-04-21 20:04:54 +0530

    fix minor errors

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import "./App.css";
 import { useDispatch } from "react-redux";
 import { addUserInfo } from "./features/userSlice";
+import { BACKGROUND_IMG } from "./utils/constants";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -23,7 +24,10 @@ const App = () => {
     });
   }, []);
   return (
-    <div className="bg-[url(https://assets.nflxext.com/ffe/siteui/vlv3/9f46b569-aff7-4975-9b8e-3212e4637f16/453ba2a1-6138-4e3c-9a06-b66f9a2832e4/IN-en-20240415-popsignuptwoweeks-perspective_alpha_website_large.jpg)] bg-cover bg-center h-screen md:h-auto ">
+    <div
+      style={{ backgroundImage: `url(${BACKGROUND_IMG})` }}
+      className="bg-cover bg-center h-screen md:h-auto "
+    >
       <Header />
       <Outlet />
     </div>
